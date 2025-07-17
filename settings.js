35a0afafa927d11d91d432bfb1cbed105f0c2e4e
Vinpinkley666 <166421298+Vinpink2@users.noreply.github.com>
2025-07-17 18:00:37 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  author: 'suprem',
+  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botOwner: 'Suprem', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;
