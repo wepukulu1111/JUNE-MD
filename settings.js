341235fb4e281b38c6eb0121560cea291a04ea27
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-07-23 09:59:04 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  author: 'supreme',
+  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botOwner: 'Suprem', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
